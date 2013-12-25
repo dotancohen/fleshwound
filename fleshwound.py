d20e2d3e8c373d19fd8e29fe42537d9c301b5848
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-12-25 12:15:17 +0200

    Iterate over all files.

--- fleshwound.py
+++ fleshwound.py
@@ -1,9 +1,24 @@
+#!/usr/bin/python3
+
 import os
+import shutil
 import sys
 
 
 
+def parse_file(f):
+
+	print(f)
+
+	return True
+
+
+
 def main():
+
+	for rootDir, folders, files in os.walk(os.getcwd()):
+		for f in files:
+			parse_file(f)
 
 	return True
 
