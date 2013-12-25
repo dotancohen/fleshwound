99066433af1384e6d0366a0a40584aaee90ee101
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-12-25 12:37:22 +0200

    Prototype that parses the correct files and types.

--- fleshwound.py
+++ fleshwound.py
@@ -4,25 +4,51 @@ import os
 import shutil
 import sys
 
+from pprint import pprint
+
+
+
+filetypes_to_read = ['.php']
+files_to_avoid = []
+
 
 
 def parse_file(f):
 
-	print(f)
+	input = open(f, 'r')
+	""" Not using try yet to learn about possible exceptions
+	try:
+		input = open(f, 'r')
+	except Exception as e:
+		pass
+
+		IOError: [Errno 2] No such file or directory: 'index']
+		Thrown when trying to parse file that does not exits ('index' instead of '.git/index')
+
+	"""
+
+	for l in input:
+		if '_GET' in l:
+			print("\n%s" % (f,))
+			print("Found _GET!")
+			print(l)
+
+	input.close()
 
 	return True
 
 
 
-def main():
+def main(files_to_avoid, filetypes_to_read):
 
-	for rootDir, folders, files in os.walk(os.getcwd()):
+	for rootDir, dirs, files in os.walk(os.getcwd()):
 		for f in files:
-			parse_file(f)
+			if not f in files_to_avoid and any(f.endswith(x) for x in filetypes_to_read):
+				parse_file(os.path.join(rootDir, f))
 
 	return True
 
 
 
 if __name__ == '__main__':
-	main()
+	main(files_to_avoid, filetypes_to_read)
